ff0eba7c90ccea9b8a29edc92f4deb817ae13417
Steven Hawking <jonathan.bittan9@gmail.com>
2026-03-03 12:08:42 +0200

    fix3

--- exe7.py
+++ exe7.py
@@ -2,7 +2,7 @@ total_fuel=0
 sum=0
 sum_total=0
 for drivers in range(30):   
-    distance=int(input("Enter a distance:"))
+    distance=int(input("Enter a distance of km:"))
     while distance!=-1 :
         sum=sum+distance
         sum_total=sum_total+distance
@@ -10,6 +10,8 @@ for drivers in range(30):
         distance=int(input("Enter a distance:"))
     print("you drove:" , sum , "kilometers")
     print("you used" , (total_fuel/10) , "liters")
+    sum=0
+    total_fuel=0
 print("all the drivers  drove:" , sum_total , "kilometers")
 
 
